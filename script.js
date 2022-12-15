6c66340d2533712765aec6930b3740b5df0bca9d
berger <robertberger.dev@gmail.com>
2022-12-15 10:50:18 -0500

    array methods setup

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var menuItem1 = "Hamburger";
-var menuItem2 = "Fries";
-var menuItem3 = "Milkshake";
-var menuItem4 = "Garden Salad";
-var menuItem5 = "Pizza Slice";
+// var menuItem1 = "Hamburger";
+// var menuItem2 = "Fries";
+// var menuItem3 = "Milkshake";
+// var menuItem4 = "Garden Salad";
+// var menuItem5 = "Pizza Slice";
 
-let menu = [
-    "Hamburger",
-    "Fries",
-    "Milkshake",
-    "Garden Salad",
-    "Pizza Slice"
-];
+// let menu = [
+//     "Hamburger",
+//     "Fries",
+//     "Milkshake",
+//     "Garden Salad",
+//     "Pizza Slice"
+// ];
 // console.log(menu);
 // console.log(menu[1]);
 
-menu[5] = "Steak";
-console.log(menu);
+// menu[5] = "Steak";
+// console.log(menu);
 
 // let menu = new Array(
 //     "Hamburger",
