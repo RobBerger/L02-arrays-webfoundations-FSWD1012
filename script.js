c5b97fe9e05641d0783e32410b525b896db97975
berger <robertberger.dev@gmail.com>
2022-12-14 22:38:45 -0500

    Added examples

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ var menuItem2 = "Fries";
 var menuItem3 = "Milkshake";
 var menuItem4 = "Garden Salad";
 var menuItem5 = "Pizza Slice";
+
+// let menu = [
+//     "Hamburger",
+//     "Fries",
+//     "Milkshake",
+//     "Garden Salad",
+//     "Pizza Slice"
+// ];
+
+let menu = new Array(
+    "Hamburger",
+    "Fries",
+    "Milkshake",
+    "Garden Salad",
+    "Pizza Slice"
+);
+
